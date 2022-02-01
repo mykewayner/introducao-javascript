a58bcd98824e51225a93baff8cf8fa5fc574a144
mykewayner <mykewayner@gmail.com>
2022-02-01 04:27:42 -0300

    Mudança geral

---
+++ js/principal.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+console.log(tdPeso)
