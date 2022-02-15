efca982fe777297f3a82c583e3c97c318fec2bdc
mykewayner <mykewayner@gmail.com>
2022-02-15 13:15:45 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,16 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
 
 
-var paciente = document.querySelector("#primeiro-paciente");
+var paciente = document.querySelector(".paciente");
+
 var tdPeso = paciente.querySelector(".info-peso");
-console.log(tdPeso)
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+var tdIMC = paciente.querySelector(".info-imc")
+
+tdIMC.textContent= imc ;
