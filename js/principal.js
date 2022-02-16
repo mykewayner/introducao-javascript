f7e4cd2c3e2d4f183171c55050c4f8ce625a7a6d
mykewayner <mykewayner@gmail.com>
2022-02-16 19:48:43 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -10,7 +10,26 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura")
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
+
 var tdIMC = paciente.querySelector(".info-imc")
 
-tdIMC.textContent= imc ;
+var pesoValido = true;
+var alturaValido = true;
+
+
+if(peso < 0 || peso > 1000){
+    tdIMC.textContent= "peso invalido"
+    pesoValido = false;
+}
+
+if (altura < 0 || altura > 3){
+    tdIMC.textContent= "altura invalida"
+    alturaValido = false
+
+}
+
+
+if(alturaValido && pesoValido){
+    var imc = peso / (altura * altura);
+    tdIMC.textContent= imc ;    
+}
